0cbd5bd9e08f7b288d82601e3463e0624a28cb02
Jonas Merforth <jonas.merforth@gmail.com>
2021-06-17 12:28:14 +0200

    Initial commit MEAN Stack

---
+++ MEAN/server/mongo.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+
+async function setMongo(): Promise<any> {
+  let mongodbURI;
+  if (process.env.NODE_ENV === 'test') {
+    mongodbURI = process.env.MONGODB_TEST_URI;
+  } else {
+    mongodbURI = process.env.MONGODB_URI;
+  }
+  mongoose.set('useCreateIndex', true);
+  mongoose.set('useNewUrlParser', true);
+  mongoose.set('useFindAndModify', false);
+  mongoose.set('useUnifiedTopology', true);
+  // Connect to MongoDB using Mongoose
+  await mongoose.connect(mongodbURI);
+  console.log('Connected to MongoDB');
+}
+
+export default setMongo;
