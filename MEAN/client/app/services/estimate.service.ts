8b89e7573f4737e5e7dd11d4de3d1dbe20c5c0ea
Jonas Merforth <jonas.merforth@gmail.com>
2021-06-17 23:27:30 +0200

    Make return estimate work, disable house

--- MEAN/client/app/services/estimate.service.ts
+++ MEAN/client/app/services/estimate.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Cat} from "../shared/models/cat.model";
-import {RealEstateProperties} from "../shared/models/realEstateProperties.model";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cat } from '../shared/models/cat.model';
+import { RealEstateProperties } from '../shared/models/realEstateProperties.model';
+
+export type estimateResult = { estimate: number };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EstimateService {
-
   constructor(private http: HttpClient) {}
 
-  getEstimate(filters: RealEstateProperties): Observable<number> {
+  getEstimate(filters: RealEstateProperties): Observable<estimateResult> {
     return this.http.post<any>('/api/estimate', filters);
   }
 }
